971195b50150bc00af01be4336669975ea2d6434
r-4v <pravaspraveen777@gmail.com>
2022-12-22 21:17:57 -0500

    added pagination support for scraping indeed

--- routes/indeed.js
+++ routes/indeed.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const indeed_scrape = require('../scraper/indeed_scraper');
 router.get("/",async(req,res)=>{
     res.send("You've reached indeed endpoint");
-    const indeed_scrape_data =  await indeed_scrape("sde");
+    await indeed_scrape("web dev");
 });
 
 module.exports =  router;
--- scraper/indeed_scraper.js
+++ scraper/indeed_scraper.js
@@ -2,15 +2,35 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 async function indeed_scrape(searchQuery){
     const jobList = [];
-    const fetchedData = await (async () => {
+    const fetchedJobList = [];
+    //const fetchedData = 
+    await (async () => {
        try{ 
         const browser = await puppeteer.launch({headless:false,defaultViewport:{ width: 1366, height: 768},args:['--start-maximized' ]});
         const page = await browser.newPage();
         await page.goto(`https://www.indeed.com/jobs?q=${searchQuery}`);
         const jobListSelector = '.jobsearch-ResultsList .resultContent';
-        //write a recursive function to make this work with pagination
-        const jobData =  page.evaluate((jobList,jobListSelector)=>{
+       function accumulateAllPages(fetchedData)
+       {
+            if(!fetchedData) return;
+            fetchedData.forEach(job=>{
+                fetchedJobList.push(job);
+            });
+       }
+        // function to write fetched data to file
+        function writeToFile(fetchedData){
+            if(!fetchedData) return;
+            const fetchedDataJson =  JSON.stringify(fetchedData,null,2);
+            fs.writeFile('indeed_job_listings.json',fetchedDataJson,'utf8',()=>{
+             console.log("data written to indeed_job_listings.json");
+            });
+          }
+        //recursive function
+       async function fetchCurrentPageData(){
+            const currentPageJobData = await page.evaluate(async (jobList,jobListSelector)=>{
             const jobListElements =  document.querySelectorAll(jobListSelector);
+            const nextPageLink  = document?.querySelector('a[data-testid = "pagination-page-next"]')?.href;
+            if(!nextPageLink) return null;
             jobListElements.forEach((element)=>{
                     let jobTitle = element.querySelector(".jobTitle span").innerText;
                     let companyName = element.querySelector(".companyName a")?.innerText;
@@ -27,22 +47,28 @@ async function indeed_scrape(searchQuery){
                         location:location,
                         salary:salary});
             });
-            return jobList;
+            return {jobList,nextPageLink};
         },jobList,jobListSelector);
+        //accumulating current Page data into fetchedJobList variable to write it to a file once the browser is closed
+        accumulateAllPages(currentPageJobData?.jobList);
+        if(currentPageJobData?.nextPageLink) 
+        {
+            console.log("Going to next Page!");
+            await page.goto(currentPageJobData?.nextPageLink);
+            // calling it recursively if next page is available
+            await fetchCurrentPageData();
+
+        }
+       }
+       //calling to eval the first page
+       await fetchCurrentPageData();
         await browser.close();
-        //console.log(jobData);
-        return jobData;
+        writeToFile(fetchedJobList);
+        
     }catch(err){
         console.log(err)
     }
     })();
-   //console.log("fetchedData is: ");
-   //console.log(fetchedData);
-   // write to file system
-   const fetchedDataJson =  JSON.stringify(fetchedData,null,2);
-   fs.writeFile('indeed_job_listings.json',fetchedDataJson,'utf8',()=>{
-    console.log("data written to indeed_job_listings.json");
-   });
-   return fetchedData;
+
 };
 module.exports = indeed_scrape;
